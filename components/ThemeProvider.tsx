b3ca5d228a9e0c1f767ce3f088cc1e01e9a51e9a
AyyubAbdurrahman <gamingayyub3@gmail.com>
2025-09-06 15:29:30 +0700

    update

--- components/ThemeProvider.tsx
+++ components/ThemeProvider.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import * as React from "react";
-import {
-  ThemeProvider as NextThemesProvider,
-  type ThemeProviderProps,
-} from "next-themes";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+
+// Turunkan tipe props langsung dari komponen penyedianya
+type ThemeProviderProps = React.ComponentProps<typeof NextThemesProvider>;
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
